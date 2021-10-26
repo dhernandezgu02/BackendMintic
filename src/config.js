42c78c9802d12300d377bad44e197bfa036dd4e9
Daniel Hernandez <dhernandezgu02@uninpahu.edu.co>
2021-10-26 01:29:30 -0500

    my pc as mongo

--- src/config.js
+++ src/config.js
@@ -3,11 +3,14 @@
 
 const configurations = {
   PORT: process.env.PORT || 4000,
-  MONGODB_HOST: process.env.MONGODB_HOST || "localhost",
-  MONGODB_DATABASE: process.env.MONGODB_DB || "products-app",
-  MONGODB_URI: `mongodb://${process.env.MONGODB_HOST || "localhost"}/${
-    process.env.MONGODB_DATABASE || "products-app"
-  }`,
+  // MONGODB_HOST: process.env.MONGODB_HOST || "localhost",
+  // MONGODB_DATABASE: process.env.MONGODB_DB || "products-app",
+  // MONGODB_URI: `mongodb://${process.env.MONGODB_HOST || "localhost"}/${
+  //   process.env.MONGODB_DATABASE || "products-app"
+  // }`,
+  MONGODB_HOST: "192.168.0.4",
+  MONGODB_DATABASE: "products-app",
+  MONGODB_URI: `mongodb://${"192.168.0.4"}/${"products-app"}`,
 };
 
 module.exports = configurations;
